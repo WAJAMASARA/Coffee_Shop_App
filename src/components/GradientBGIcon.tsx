f3f2b3bba304c080758e06d1db46cea381265d48
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2024-02-01 10:28:15 +0530

    create Gradient component

---
+++ src/components/GradientBGIcon.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const GradientBGIcon = () => {
+  return (
+    <View>
+      <Text>GradientBGIcon</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default GradientBGIcon;
